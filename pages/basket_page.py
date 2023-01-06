87057ad307ef30964e69485019d21fccae785d0d
Denis Novoselov <d.novoselov@arenadata.io>
2023-01-06 15:02:20 +0500

    4.3.10 done

---
+++ pages/basket_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def __init__(self, *args, **kwargs):
+        super(BasketPage, self).__init__(*args, **kwargs)
+
+    def basket_is_empty(self):
+        assert self.is_not_element_present(
+            *BasketPageLocators.BASKET_PAGE_ITEMS
+        ), "Basket contains items, but it should not"
+
+    def basket_empty_message_exists(self):
+        assert self.is_element_present(
+            *BasketPageLocators.BASKET_IS_EMPTY_MESSAGE
+        ), "Message that Basket is empty does not exist"
--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini .btn-default:first-child")
 
 
 class LoginPageLocators:
@@ -18,3 +19,8 @@ class ProductPageLocators:
     PRODUCT_PRICE_ADDED_MESSAGE = (By.XPATH, "//div[@id='messages']/div[3]")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main > .price_color ")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages .alert-success:first-child")
+
+
+class BasketPageLocators:
+    BASKET_PAGE_ITEMS = (By.CSS_SELECTOR, ".basket-items")
+    BASKET_IS_EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner")
